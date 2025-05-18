cf347cd8883e989cc7fdb4bb44553e5e78228e39
Gabriel Marreiros <gabriel.s.marreiros@hotmail.com>
2025-05-18 15:54:53 -0300

    refactor(backend/tests): modifcações nos testes unitários para atender as modificações realizadas na nova versão do sistema

--- backend/src/test/java/br/com/gabrielmarreiros/backend/repositories/CustomerRepositoryTest.java
+++ backend/src/test/java/br/com/gabrielmarreiros/backend/repositories/CustomerRepositoryTest.java
@@ -33,24 +33,30 @@ class CustomerRepositoryTest {
         @Test
         @DisplayName("")
         void whenRequesting_thenInactiveUser() {
+//          Arrange
+            UUID customerId = UUID.fromString("8f069839-6943-4c4a-ae8f-6581d9dcf42c");
+
 //          Action
-            customerRepository.changeCustomerActiveStatus(UUID.fromString("8f069839-6943-4c4a-ae8f-6581d9dcf42c"));
-            Customer repositoryResponse = customerRepository.findById(UUID.fromString("1ec17e60-48b6-457e-8597-54e20c806ef9")).orElse(null);
+            customerRepository.changeCustomerActiveStatus(customerId);
+            Customer repositoryResponse = customerRepository.findById(customerId).orElse(null);
 
 //          Assert
-            assertThat(repositoryResponse.getUser().getUserStatus())
+            assertThat(repositoryResponse.getUserStatus())
                     .isEqualTo(UserStatusEnum.INACTIVE.getValue());
         }
 
         @Test
         @DisplayName("")
         void whenRequesting_thenActiveUser() {
+//          Arrange
+            UUID customerId = UUID.fromString("6c2071db-3ebe-49a1-84a8-f728c7d9f45d");
+
 //          Action
-            customerRepository.changeCustomerActiveStatus(UUID.fromString("6c2071db-3ebe-49a1-84a8-f728c7d9f45d"));
-            Customer repositoryResponse = customerRepository.findById(UUID.fromString("42141baf-d7a1-425e-84b3-e6bd3c5e3e93")).orElse(null);
+            customerRepository.changeCustomerActiveStatus(customerId);
+            Customer repositoryResponse = customerRepository.findById(customerId).orElse(null);
 
 //          Assert
-            assertThat(repositoryResponse.getUser().getUserStatus())
+            assertThat(repositoryResponse.getUserStatus())
                     .isEqualTo(UserStatusEnum.ACTIVE.getValue());
         }
     }
